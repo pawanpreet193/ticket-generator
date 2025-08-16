bfd183a7525375c28454d4c18058b0f85998ac70
pawanpreet193 <pawanpreetsingh193@gmail.com>
2025-08-16 21:23:12 +0530

    error messages

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const form = document.querySelector('form');
 const ticket = document.getElementById('ticket');
+const avatarInput = document.getElementById('avatar');
+const errorMessage = document.getElementById("avatar-error");
+const emailInput = document.getElementById('email');
+
+
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -13,9 +18,50 @@ form.addEventListener('submit', function(event) {
     ticket.querySelector('#github-username-output').textContent = github;
     ticket.querySelector('#avatar-name-output').textContent = name;
 
-
+   if (avatar) {
+       
+        if (avatar.size <= 500 * 1024) {
+            const avatarURL = URL.createObjectURL(avatar);
+            ticket.querySelector(".avatar-placeholder").src = avatarURL;
+        } else {
+            alert("Avatar image must be less than 500 KB.");
+            document.getElementById('avatar').value = "";
+            return; 
+        }
+    }
     
     ticket.classList.remove('hidden');
     document.querySelector(".initial").classList.add("hidden");
 
+});
+
+avatarInput.addEventListener("change", function() {
+    const avatar = avatarInput.files[0];
+    if (avatar) {
+        if (avatar.size > 500 * 1024) {
+            errorMessage.classList.remove("hidden");
+            document.querySelector(".avatar-info").classList.add("hidden");
+            document.querySelector(".info-icon").classList.add("hidden");
+            avatarInput.value = "";
+            return;
+        }
+        errorMessage.classList.add("hidden"); 
+        const avatarURL = URL.createObjectURL(avatar);
+        document.querySelector(".upload-icon").src = avatarURL;
+    
+       
+    }
+});
+
+emailInput.addEventListener("input", function() {
+   
+    const emailError = document.getElementById("email-error");
+    
+    if (!emailInput.checkValidity()) {
+    emailError.classList.remove("hidden");
+    emailInput.style.border = "2px solid hsl(7, 88%, 67%)"; 
+} else {
+    emailError.classList.add("hidden");
+    emailInput.style.border = "1px solid hsl(245, 15%, 58%)";
+}
 });
